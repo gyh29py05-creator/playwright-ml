e19eb43d7d76c631a9210f23b2a9608596de1ed1
gyh29py05-creator <gyh29py05@gmail.com>
2026-05-17 17:13:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -139,11 +139,8 @@ app.get("/ofertas", async (req, res) => {
       locale: "pt-BR"
     });
     const page = await context.newPage();
-    await page.goto("https://www.mercadolivre.com.br/ofertas?category=MLB1574#filter_applied=category&filter_position=4&origin=qcat",
+await page.goto("https://www.mercadolivre.com.br/ofertas?category=MLB1574#filter_applied=category&filter_position=4&origin=qcat",
 {
-  waitUntil: "domcontentloaded",
-  timeout: 30000
-});
   waitUntil: "domcontentloaded",
   timeout: 30000
 });
